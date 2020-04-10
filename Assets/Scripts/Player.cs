50f44960ab95cda855f15640ca4bf977e11b55a8
William AbouSharaf <liams_1998@hotmail.com>
2020-04-10 22:15:03 +0300

    Added Player movement functionality + prefab

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed = 3.0f;
+    public int maxHealth = 5;
+    int currentHealth;
+    Vector2 lookDirection = new Vector2(1, 0);
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+    }
+
+    private void Update()
+    {
+        HandlePlayerMovement();
+    }
+
+    private void HandlePlayerMovement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        Vector2 position = rb.position;
+
+        position = position + move * speed * Time.deltaTime;
+
+        rb.MovePosition(position);
+    }
+
+    public void playerTakeDamae(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+    }
+}
